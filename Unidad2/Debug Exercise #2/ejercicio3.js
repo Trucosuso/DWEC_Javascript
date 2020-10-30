24917d738733c65cc66dcb406082cfb63a7af300
Jesús L. Pinzolas <jesus@trucosuso.com>
2020-10-30 20:45:05 +0100

    debug excersice 2 finished

--- Unidad2/Debug Exercise #2/ejercicio3.js
+++ Unidad2/Debug Exercise #2/ejercicio3.js
@@ -1,22 +1,22 @@
 // WORKSHEET 4.1
 
 // EJERCICIO 3 -----------------------------------------------------
-function muestraError(objPositionError)
-{
-    switch (objPositionError.code)
-    {
+var distancia, direccion, puntoAnterior, puntoActual, map, geocoder, content, miPosicion;
+
+function muestraError(objPositionError) {
+    switch (objPositionError.code) {
     case objPositionError.PERMISSION_DENIED:
         content.innerHTML = "No se ha permitido el acceso a la posición del usuario.";
         break;
-		
+
     case objPositionError.POSITION_UNAVAILABLE:
         content.innerHTML = "No se ha podido acceder a la información de su posición.";
         break;
-		
+
     case objPositionError.TIMEOUT:
         content.innerHTML = "El servicio ha tardado demasiado tiempo en responder.";
         break;
-		
+
     default:
         content.innerHTML = "Error desconocido.";
     }
@@ -24,46 +24,46 @@ function muestraError(objPositionError)
 
 function ejercicio3() {
     content = document.getElementById("contenido");
-    geocoder = new google.maps.Geocoderr;
-    
-    map = new google.maps.Map(document.getElementById('mapa'), {
-        centre: {lat: 0, lng: 0},
+    geocoder = new google.maps.Geocoder();
+
+    map = new google.maps.Map(document.getElementById("mapa"), {
+        center: { lat: 0, lng: 0 },
         zoom: 18
     });
     miPosicion = new google.maps.Marker({
-        position: {lat: 0, lng: 0},
+        position: { lat: 0, lng: 0 },
         map: map,
         animation: google.maps.Animation.BOUNCE_DROP,
-        title: 'Que chulo es esto!'
+        title: "Que chulo es esto!"
     });
-    
+
     distancia = 0;
     direccion = "";
     puntoAnterior = null;
     puntoActual = null;
-    
+
     let opciones = {
         enableHighAccuracy: true,
-        timeout: 5,
-        maximumAge: 1,
-        minimumHighSupport: false
+        timeout: 5000,
+        maximumAge: 1000,
+        //minimumHighSupport: false
     };
-    
+
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(muestraPosicionContinua, muestraError, opciones);
     }
 }
-function muestraPosicionContinua(posicion)
-{
+
+function muestraPosicionContinua(posicion) {
     var long = posicion.coords.longitude;
     var lati = posicion.coords.latitude;
 
-    var puntoActual={lat: lati, lng: long};
+    puntoActual = { lat: lati, lng: long };
     if (puntoAnterior)
-        distancia+=calculaDistancia(puntoAnterior,puntoActual)
+        distancia += calculaDistancia(puntoAnterior, puntoActual);
     else
-        puntoAnterior=puntoActual;
-    
+        puntoAnterior = puntoActual;
+
     // Pintar recorrido -> Linea entre puntos
     var line = new google.maps.Polyline({
         map: map,
@@ -72,52 +72,49 @@ function muestraPosicionContinua(posicion)
         strokeOpacity: 0.8,
         strokeColor: "#FFAA00"
     });
-    
-    puntoAnterior= puntoActual;
-    
-    
+
+    puntoAnterior = puntoActual;
+
+
     // Mostrar la dirección
-    geocoder.geocode({'location': puntoActual}, function(results, status) {
-        if (status === google.maps.GeocoderStatus.OK) 
-        {
-            if (results[12])
-                direccion= results[0].formatted_address;
-            else 
-                direccion = 'No results found';
-        } else 
-            direccion = 'Geocoder failed due to: ' + status;
-            
+    geocoder.geocode({ "location": puntoActual }, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            if (results[0])
+                direccion = results[0].formatted_address;
+            else
+                direccion = "No results found";
+        } else
+            direccion = "Geocoder failed due to: " + status;
+
         document.getElementById("direccion").innerHTML = "<p><strong>Direccion:</strong> " + direccion;
     });
-   
-    content.innerHTML ="<p><strong>Latitud:</strong> " + lati + "</p><p><strong>Longitud:</strong> " + long + "</p><p><strong>Distancia:</strong> " + distancia; 
-    
+
+    content.innerHTML = "<p><strong>Latitud:</strong> " + lati + "</p><p><strong>Longitud:</strong> " + long + "</p><p><strong>Distancia:</strong> " + distancia;
+
     // Mostrar posicion en mapa
     map.panTo(puntoActual);
     // Actualiza marcador
     miPosicion.setPosition(puntoActual);
-    
-   
+
+
 }
 
-function calculaDistancia(location1, location2)
-{
+function calculaDistancia(location1, location2) {
     var R = 6371;
-    var dLat = toRadianes(location2.lat-location1.lat);
-    var dLon = toRadianes(location2.lng-location1.lng);
+    var dLat = toRadianes(location2.lat - location1.lat);
+    var dLon = toRadianes(location2.lng - location1.lng);
     var dLat1 = toRadianes(location1.lat);
     var dLat2 = toRadianes(location2.lat);
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
         Math.cos(dLat1) * Math.cos(dLat1) *
-        Math.sin(dLon/2) * Math.sin(dLon/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     var d = R * c;
-    
+
     return d;
 }
 
-function toRadianes(valor)
-{
+function toRadianes(valor) {
     return valor * Math.PI / 180;
 }
 
